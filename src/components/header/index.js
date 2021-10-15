e330b817cf961f2d0443317b020118565e2593c1
zagamendes <zagamendes@users.noreply.github.com>
2021-10-15 01:14:05 -0300

    final settings adjusted

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,17 +1,27 @@
 import React from "react";
-import './header.css'
-import {ReactComponent as Logo} from '../../img/logo.svg'
-import { Link } from "react-router-dom";
-
-
-const Header = ({isBlack}) => {
+import "./header.css";
+import { ReactComponent as Logo } from "../../img/logo.svg";
+import { useHistory } from "react-router-dom";
+const Header = ({ isBlack }) => {
+  const history = useHistory()
+  const logout = ()=>{
+    localStorage.removeItem("user")
+    history.push("/")
+  }
   return (
-    <header className={`header-browse ${ isBlack==true ? 'black-header' : ''}`}>
+    <header
+      className={`header-browse ${isBlack == true ? "black-header" : ""}`}
+    >
       <div className="logo">
         <Logo />
       </div>
       <div className="ctn-btn-entrar">
-        <img src="https://occ-0-417-3852.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABUCZYfPbupvQjzSa3egePk8TFNDy2A_w15DEAq50IqW8MYmOtmbWwN4Txem7mgNYEMPJ1BY6uasiIJQ8JeFO3EU.png?r=b97" />
+        <div>
+          <img src="https://occ-0-417-3852.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABUCZYfPbupvQjzSa3egePk8TFNDy2A_w15DEAq50IqW8MYmOtmbWwN4Txem7mgNYEMPJ1BY6uasiIJQ8JeFO3EU.png?r=b97" />
+        </div>
+        <div className="logout" onClick={logout}>
+          Sair
+        </div>
       </div>
     </header>
   );
--- src/components/telas/login/index.js
+++ src/components/telas/login/index.js
@@ -4,12 +4,14 @@ import Footer from "../../footer";
 import bgLarge from "../../../img/bg-large.jpg";
 import { useHistory } from "react-router";
 import isLogged from "../../../utils/auth";
-import { getDatabase, ref, onValue, onChildAdded } from "firebase/database";
+import { getDatabase, ref, onChildAdded, onValue } from "firebase/database";
 import { usuarioContext } from "../../../utils/contexto";
+import Cached from "@material-ui/icons/Cached";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
+  const [loading, setLoading] = useState(false);
   const history = useHistory();
   const { user, setUser } = useContext(usuarioContext);
   const db = getDatabase();
@@ -18,30 +20,27 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
+    onValue(usuariosRef, (snapshot) => {
+      const IDs = Object.keys(snapshot.val());
+      const usuarios = snapshot.val();
 
-    /* onValue(
-      usuariosRef,
-      (snapshot) => {
-        console.log(snapshot.val());
-        const usuarios = snapshot.val()
-        const dados = Object.keys(usuarios)
-        dados.forEach(id => {
-          console.log(usuarios[id].email)
-          
-        });
-        /* snapshot.val().forEach((usuario) => {
-          if (usuario.email == email && usuario.senha == senha)
-            setUser({ ...usuario, id: usuario.key });
-        });
-        return false;
-        history.push("/browse"); 
-      },
-      {
-        onlyOnce: true,
+      IDs.forEach((id) => {
+        if (usuarios[id].email == email && usuarios[id].senha == senha) {
+          setUser({ ...usuarios[id], id: id });
+          localStorage.setItem("user", JSON.stringify({...user}));
+          history.push("/browse");
+        }
+      });
+      setLoading(false);
+    });
+
+    onChildAdded(usuariosRef, (usuario) => {
+      if (usuario.email == email && usuario.senha == senha) {
+        setUser({ ...usuario, id: usuario.key });
+        setLoading(false);
+        history.push("/browse");
       }
-    ); */
-    onChildAdded(usuariosRef, (data) => {
-      console.log(data.key)
     });
   };
   return (
@@ -63,7 +62,7 @@ const Login = () => {
                 type="password"
                 onChange={({ target }) => setSenha(target.value)}
               />
-              <button>Entrar</button>
+              <button>Entrar {loading ? <Cached /> : ""}</button>
             </form>
           </div>
         </div>
